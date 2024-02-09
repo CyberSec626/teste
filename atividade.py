4720e5aadfcef26c26a5500e2e8428115ccb6c00
Amaraí Santos <125764914+CyberSec626@users.noreply.github.com>
2024-02-09 17:10:07 -0300

    Delete atividade.py

--- atividade.py
+++
@@ -1,6 +0,0 @@
-nome=input('Informe seu nome completo: ')
-idade=input("Informe sua idade (apenas número): ")
-mensagem = 'Seu nome é '+nome+ ' e sua idade é '+idade +' anos'
-mensagem2 =  'Seu nome é {} e sua idade é {} anos'.format(nome, idade)
-print(mensagem)
-print(mensagem2)
