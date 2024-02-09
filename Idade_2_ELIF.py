932619b3ad89224c998e2899aa661c8b0b20011f
Amaraí Santos <125764914+CyberSec626@users.noreply.github.com>
2024-02-09 17:09:31 -0300

    Delete Idade_2_ELIF.py

--- Idade_2_ELIF.py
+++
@@ -1,8 +0,0 @@
-idade = 41
-palpite = int(input('Descubra minha idade: '))
-if (palpite == idade):
-  print('Acertou!')
-elif (palpite > idade):
-  print('Errou! Seu palpite foi maior que a minha idade')
-elif (palpite < idade):
-  print('Errou! Seu palpite foi menor que a minha idade')
