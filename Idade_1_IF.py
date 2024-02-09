406b91f5aa3ac46a6ebbc762998ffaebbc668104
Amaraí Santos <125764914+CyberSec626@users.noreply.github.com>
2024-02-09 17:03:41 -0300

    Add files via upload

---
+++ Idade_1_IF.py
@@ -0,0 +1,8 @@
+idade = 41
+palpite = int(input('Descubra minha idade: '))
+if (palpite == idade):
+  print('Acertou!')
+else:
+  print('Errou!')
+
+
---
+++ erro.py
@@ -0,0 +1,3 @@
+nota=10
+mensagem="Os alunos da AcaditiTI avaliam o curso com nota: "
+print(mensagem+nota)
